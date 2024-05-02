67fce73952f4245650aaf6007aac7c3d8217abe3
Carlos Barillas <giovanib07@gmail.com>
2024-05-02 12:46:39 -0600

    feat:adding status code

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,20 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-let tasks = [];
+let tasks = [{
+    'id':'1',
+    'name':'caminar al perro',
+    'description':'Llevar al perro al parque',
+    'dueDate':'2024-04-20'
+}];
 
 /* GET users listing. */
 router.get('/getTasks', function(req, res, next) {
-    res.json(tasks)
+    res.status(200).json(tasks)
 });
 
 router.delete('/removeTask/:id', function(req, res, next) {
+    console.log(req.params.id);
     if(req.params && req.params.id){
         let id = req.params.id;
-        tasks = tasks.filter(task => task.id == id); 
-        res.json(tasks)
+        tasks = tasks.filter(task => task.id !== id); 
+        res.status(200).json(tasks)
     }else {
-        es.json({})
+        res.status(400).json({})
     }
 });
 
@@ -24,8 +30,11 @@ router.post('/addTask', function(req, res, next) {
     if(req.body && req.body && req.body.name && req.body.description && req.body.description){
         req.body.id = timestamp;
         tasks.push(req.body);
+        res.status(200).json(tasks);
+    }else{
+        res.status(400).json(tasks);
     }
-    res.json(tasks)
+
    // res.send('respond with a resource');
 });
 
